d5c4cfdedba684b71a7b7445e550b65314a54ad1
weldsh2535 <weldetsadik2535@gmail.com>
2023-01-26 17:48:58 +0300

    pass data from one activity to other activity using intnent

--- app/src/main/java/com/weldsh2535/shareitapps/MainActivity.kt
+++ app/src/main/java/com/weldsh2535/shareitapps/MainActivity.kt
@@ -15,13 +15,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btSendMessage.setOnClickListener {
+        binding.btnSendMessage.setOnClickListener {
             val message = binding.tvMessage.text.toString()
 
             Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
             val intent = Intent(this,SecondActivity::class.java)
             intent.putExtra("sendMessage",message)
             startActivity(intent)
+        }
+
+        binding.btnShareMessage.setOnClickListener {
+            val message = binding.tvMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT,message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent,"Share It"))
         }
     }
 }
